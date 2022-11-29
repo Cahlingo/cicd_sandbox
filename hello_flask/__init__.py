4dabe7db0ed1d51d9dc36612f3cd1a791ec9cbf0
ritval <klas@playgroundtech.io>
2022-11-29 14:31:01 +0100

    lmao

---
+++ hello_flask/__init__.py
@@ -0,0 +1,3 @@
+"""Amazing website lmao."""
+
+from .app import app
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,4 +1,6 @@
-""" from app import hello_world
+"""This module does blah blah."""
+from app import hello_world
 
 def test_returns_hello_world():
-    assert hello_world() == 'Hello, World!' """
+    """This module does blah blah."""
+    assert hello_world() == 'Hello, World!'
